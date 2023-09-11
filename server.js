c3050d79dc2eee0259723f6ad9effc6f8d767bfb
Yank2901 <yanick290101@gmail.com>
2023-09-11 16:07:40 -0500

    Implementacion de sockets

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
 const app = express();
 const port = 8000;
-require('./config/mongoose.config')
+require('./config/mongoose.config');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 const allUserRoutes = require('./routes/user.routes');
 allUserRoutes(app);
-app.listen(port, () => {
+
+const server = http.createServer(app);
+
+// Importa el servidor WebSocket y configúralo
+const setupWebSocketServer = require('./websocketServer');
+setupWebSocketServer(server);
+
+server.listen(port, () => {
   console.log("Server listening at port", port);
 });
---
+++ websocketServer.js
@@ -0,0 +1,51 @@
+const { Server } = require('socket.io');
+const Chat = require('./models/chat.model');
+
+function setupWebSocketServer(server) {
+  const io = new Server(server, {
+    cors: {
+      origin: '*',
+    },
+  });
+
+  io.on('connection', (socket) => {
+    console.log('Nuevo usuario conectado');
+
+    socket.on('mensaje', (data) => {
+      try {
+        Chat.create({
+          idCell: data.idCell,
+          idUser: data.idUser,
+          nameUser: data.nameUser,
+          message: data.message,
+          date: data.date,
+          typeMessage: data.typeMessage,
+        })
+          .then((chat) => {
+            socket.emit('mensaje-confirmado', {
+              idCell: data.idCell,
+              idUser: data.idUser,
+              nameUser: data.nameUser,
+              message: data.message,
+              date: data.date,
+              typeMessage: data.typeMessage,
+            });
+          })
+          .catch((err) => {
+            console.error('Error al guardar el mensaje en la base de datos:', err);
+            socket.emit('mensaje-error', { error: 'Error al guardar el mensaje en la base de datos' });
+          });
+      } catch (error) {
+        console.error('Error al procesar el mensaje:', error);
+        // En caso de un error en el procesamiento del mensaje, emite una respuesta de error al cliente
+        socket.emit('mensaje-error', { error: 'Error en el procesamiento del mensaje' });
+      }
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Usuario desconectado');
+    });
+  });
+}
+
+module.exports = setupWebSocketServer;
